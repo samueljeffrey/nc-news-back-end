dd7fb2bf55c2ac6a0aa88c6dcb52a05f13233f56
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2021-11-01 11:21:17 +0000

    drop and create tables in seed.js file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,7 +1,53 @@
+const db = require("../db");
+const format = require("pg-format");
+
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
-  // 1. create tables
-  // 2. insert data
+  return db
+    .query(`DROP TABLE IF EXISTS topics`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS articles`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS comments`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE topics (
+        slug VARCHAR(255) UNIQUE PRIMARY KEY NOT NULL,
+        description VARCHAR(255) NOT NULL      
+      )`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE users (
+        username VARCHAR(20) UNIQUE PRIMARY KEY NOT NULL,
+        AVATAR_URL VARCHAR(500),
+        name VARCHAR(255) NOT NULL
+      )`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE articles (
+        article_id SERIAL PRIMARY KEY NOT NULL,
+        title VARCHAR(100) NOT NULL,
+        body VARCHAR(10000) NOT NULL,
+        votes INT DEFAULT 0,
+        topic VARCHAR(255) REFERENCES topics(slug),
+        author VARCHAR(20) REFERENCES users(username),
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+      )`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE comments (
+        comment_id PRIMARY KEY SERIAL,
+        author VARCHAR(20) REFERENCES users(username),
+        auticle_id INT REFERENCES articles(article_id),
+        votes INT DEFAULT 0,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+        body VARCHAR (1000) NOT NULL
+      )`);
+    });
 };
 
 module.exports = seed;
