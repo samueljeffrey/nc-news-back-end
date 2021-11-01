4ea52a7b698c7daf9bfe8bfa856365f85353e72d
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2021-11-01 11:21:45 +0000

    insert data into tables in seed.js file

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -47,6 +47,47 @@ const seed = (data) => {
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP
         body VARCHAR (1000) NOT NULL
       )`);
+    })
+    .then(() => {
+      const topicQuery = format(
+        `INSERT INTO topics(slug, description) VALUES %L RETURNING *;`,
+        topicData.map((topic) => [topic.slug, topic.description])
+      );
+      return db.query(topicQuery);
+    })
+    .then(() => {
+      const userQuery = format(
+        `INSERT INTO users(username, avatar_url, name) VALUES %L RETURNING *;`,
+        userData.map((user) => [user.username, user.avatar_url, user.name])
+      );
+      return db.query(userQuery);
+    })
+    .then(() => {
+      const articleQuery = format(
+        `INSERT INTO articles(title, body, votes, topic, author, created_at) VALUES %L RETURNING *;`,
+        articleData.map((article) => [
+          article.title,
+          article.body,
+          article.votes,
+          article.topic,
+          article.author,
+          article.created_at,
+        ])
+      );
+      return db.query(articleQuery);
+    })
+    .then(() => {
+      const commentQuery = format(
+        `INSERT INTO comments(author, article_id, votes, created_at, body) VALUES %L RETURNING *;`,
+        commentData.map((comment) => [
+          comment.author,
+          comment.article_id,
+          comment.votes,
+          comment.created_at,
+          comment.body,
+        ])
+      );
+      return db.query(commentQuery);
     });
 };
 
