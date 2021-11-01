43b1dc603c62a5ac622aca51a3cd3220694a3ab2
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2021-11-01 14:48:35 +0000

    add provisionally completed router, error, controller and model files for /api and /api/topics paths, prior to beginning tests

--- routers/api-router.js
+++ routers/api-router.js
@@ -1,6 +1,7 @@
 const apiRouter = require("express").Router();
 const topicsRouter = require("./topics-router");
 const { handleUrlErrors } = require("../errors/errors.js");
+const { getApi } = require("../controllers/controller.js");
 
 apiRouter.use("/topics", topicsRouter);
 apiRouter.route("/").get(getApi);
