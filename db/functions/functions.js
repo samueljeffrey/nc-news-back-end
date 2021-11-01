0a58d8d4be1d612606ef1bbee70f2a4ea6e74cbe
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2021-11-01 14:07:53 +0000

    add completed seeding, with functions in functions.js file, testing of which is unnecessary

---
+++ db/functions/functions.js
@@ -0,0 +1,108 @@
+const db = require("../index.js");
+const format = require("pg-format");
+
+const dropTables = () => {
+  return db
+    .query(`DROP TABLE IF EXISTS comments;`)
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS articles;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS users;`);
+    })
+    .then(() => {
+      return db.query(`DROP TABLE IF EXISTS topics;`);
+    });
+};
+
+const createTables = () => {
+  return db
+    .query(
+      `CREATE TABLE topics (
+    slug VARCHAR(255) UNIQUE PRIMARY KEY NOT NULL,
+    description VARCHAR(255) NOT NULL      
+  );`
+    )
+    .then(() => {
+      return db.query(`CREATE TABLE users (
+    username VARCHAR(20) UNIQUE PRIMARY KEY NOT NULL,
+    AVATAR_URL VARCHAR(500),
+    name VARCHAR(255) NOT NULL
+  );`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE articles (
+    article_id SERIAL PRIMARY KEY NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    body VARCHAR(10000) NOT NULL,
+    votes INT DEFAULT 0,
+    topic VARCHAR(255) REFERENCES topics(slug),
+    author VARCHAR(20) REFERENCES users(username),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );`);
+    })
+    .then(() => {
+      return db.query(`CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    author VARCHAR(20) REFERENCES users(username),
+    article_id INT REFERENCES articles(article_id),
+    votes INT DEFAULT 0,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    body VARCHAR(1000) NOT NULL
+  );`);
+    });
+};
+
+const insertTopicData = (topicData) => {
+  const topicQuery = format(
+    `INSERT INTO topics(slug, description) VALUES %L RETURNING *;`,
+    topicData.map((topic) => [topic.slug, topic.description])
+  );
+  return db.query(topicQuery);
+};
+
+const insertUserData = (userData) => {
+  const userQuery = format(
+    `INSERT INTO users(username, avatar_url, name) VALUES %L RETURNING *;`,
+    userData.map((user) => [user.username, user.avatar_url, user.name])
+  );
+  return db.query(userQuery);
+};
+
+const insertArticleData = (articleData) => {
+  const articleQuery = format(
+    `INSERT INTO articles(title, body, votes, topic, author, created_at) VALUES %L RETURNING *;`,
+    articleData.map((article) => [
+      article.title,
+      article.body,
+      article.votes,
+      article.topic,
+      article.author,
+      article.created_at,
+    ])
+  );
+  return db.query(articleQuery);
+};
+
+const insertCommentData = (commentData) => {
+  const commentQuery = format(
+    `INSERT INTO comments(author, article_id, votes, created_at, body) VALUES %L RETURNING *;`,
+    commentData.map((comment) => [
+      comment.author,
+      comment.article_id,
+      comment.votes,
+      comment.created_at,
+      comment.body,
+    ])
+  );
+  return db.query(commentQuery);
+};
+
+module.exports = {
+  dropTables,
+  createTables,
+  insertTopicData,
+  insertUserData,
+  insertArticleData,
+  insertCommentData,
+};
--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,63 +1,30 @@
-const db = require("../db");
+const db = require("../index.js");
 
 const seed = (data) => {
   const { articleData, commentData, topicData, userData } = data;
-  return db
-    .query(`DROP TABLE IF EXISTS topics;`)
+  const {
+    dropTables,
+    createTables,
+    insertTopicData,
+    insertUserData,
+    insertArticleData,
+    insertCommentData,
+  } = require("../functions/functions.js");
+  return dropTables()
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS users;`);
+      return createTables();
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS articles;`);
+      return insertTopicData(topicData);
     })
     .then(() => {
-      return db.query(`DROP TABLE IF EXISTS comments;`);
+      return insertUserData(userData);
     })
     .then(() => {
-      return db.query(`CREATE TABLE topics (
-        slug VARCHAR(255) UNIQUE PRIMARY KEY NOT NULL,
-        description VARCHAR(255) NOT NULL      
-      );`);
+      return insertArticleData(articleData);
     })
     .then(() => {
-      return db.query(`CREATE TABLE users (
-        username VARCHAR(20) UNIQUE PRIMARY KEY NOT NULL,
-        AVATAR_URL VARCHAR(500),
-        name VARCHAR(255) NOT NULL
-      );`);
-    })
-    .then(() => {
-      return db.query(`CREATE TABLE articles (
-        article_id SERIAL PRIMARY KEY NOT NULL,
-        title VARCHAR(100) NOT NULL,
-        body VARCHAR(10000) NOT NULL,
-        votes INT DEFAULT 0,
-        topic VARCHAR(255) REFERENCES topics(slug),
-        author VARCHAR(20) REFERENCES users(username),
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-      );`);
-    })
-    .then(() => {
-      return db.query(`CREATE TABLE comments (
-        comment_id PRIMARY KEY SERIAL,
-        author VARCHAR(20) REFERENCES users(username),
-        auticle_id INT REFERENCES articles(article_id),
-        votes INT DEFAULT 0,
-        created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-        body VARCHAR (1000) NOT NULL
-      );`);
-    })
-    .then(() => {
-      return insertTopicData;
-    })
-    .then(() => {
-      return insertUserData;
-    })
-    .then(() => {
-      return insertArticleData;
-    })
-    .then(() => {
-      return insertCommentData;
+      return insertCommentData(commentData);
     });
 };
 
