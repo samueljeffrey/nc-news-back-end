558ed994705ef5552abda7a2de6c5c3bbafa3760
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2021-11-04 12:22:26 +0000

    add necessary heroku setup (trying again)

--- models/topics-model.js
+++ models/topics-model.js
@@ -1,7 +1,12 @@
 const db = require("../db");
 
 exports.selectTopics = () => {
-  return db.query(`SELECT * FROM topics`).then((response) => {
-    return response.rows;
-  });
+  return db
+    .query(`SELECT * FROM topics`)
+    .then((response) => {
+      return response.rows;
+    })
+    .catch((err) => {
+      return "Database is not set up";
+    });
 };
